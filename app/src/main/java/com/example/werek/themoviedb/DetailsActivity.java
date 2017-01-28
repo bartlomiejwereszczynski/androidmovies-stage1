f8c3e76cfd21f5783ad775dea1c0c92895eadc85
Bartlomiej Wereszczynski <werexx@gmail.com>
2017-01-28 18:23:00 +0100

    return to base activity without list reload

--- app/src/main/java/com/example/werek/themoviedb/DetailsActivity.java
+++ app/src/main/java/com/example/werek/themoviedb/DetailsActivity.java
@@ -2,8 +2,11 @@ package com.example.werek.themoviedb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +40,11 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         ButterKnife.bind(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+        }
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(MainActivity.MOVIE_EXTRA)) {
@@ -72,5 +80,31 @@ public class DetailsActivity extends AppCompatActivity {
                     .into(mBackdrop);
         }
         setTitle(movie.getTitle());
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     * <p>
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
